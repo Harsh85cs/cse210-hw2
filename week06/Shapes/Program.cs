a11aebfe5bdd391f89fe4e3c639811acf46c3183
Abraham Lopez <abrahamolopezp@gmail.com>
2026-04-15 19:11:38 -0600

    activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+        shapes.Add(new Square("Yellow", 8));
+        shapes.Add(new Circle("Purple", 2));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()} - Area: {shape.GetArea():F2}");
+        }
     }
 }
